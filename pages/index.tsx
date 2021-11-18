052b2122ca3018c84177a2e0f4d77a8c404bfc5c
rajinwonderland <raj@novvuminc.com>
2021-11-17 18:34:38 -0800

    chore: add demo chakra component on pages/index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { Heading } from "@chakra-ui/react";
 import Head from "next/head";
 
 export default function Home() {
@@ -11,6 +12,9 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Heading as="h1" p="4">
+        Welcome to the AreaHub React Challenge 🎉!
+      </Heading>
       {/* Your code here */}
     </>
   );
